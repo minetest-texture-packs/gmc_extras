40737a63bc6f285572958dc830dbda6a8e78d315
tacotexmex <josefsahlberg+minetest@gmail.com>
2017-04-25 19:43:38 +0200

    Sandstone block

---
+++ init.lua
@@ -0,0 +1,3 @@
+minetest.override_item("default:sandstone_block", {
+	tiles = {"default_sandstone_block.png", "extras_default_sandstone_block_bottom.png", "extras_default_sandstone_block_side.png", "extras_default_sandstone_block_side.png", "extras_default_sandstone_block_side.png", "extras_default_sandstone_block_side.png"},
+})
