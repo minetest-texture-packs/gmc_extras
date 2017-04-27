837b13d6d549906c5fb012789110d85dfa921ee1
tacotexmex <josefsahlberg+minetest@gmail.com>
2017-04-27 20:30:36 +0200

    Look for default

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
-minetest.override_item("default:sandstone_block", {
-	tiles = {"default_sandstone_block.png", "extras_default_sandstone_block_bottom.png", "extras_default_sandstone_block_side.png", "extras_default_sandstone_block_side.png", "extras_default_sandstone_block_side.png", "extras_default_sandstone_block_side.png"},
-})
+--default overrides
+if minetest.get_modpath("default") ~= nil then
+  minetest.override_item("default:sandstone_block", {
+  	tiles = {"default_sandstone_block.png",
+    "extras_default_sandstone_block_bottom.png",
+    "extras_default_sandstone_block_side.png",
+    "extras_default_sandstone_block_side.png",
+    "extras_default_sandstone_block_side.png",
+    "extras_default_sandstone_block_side.png"},
+  })
+end
