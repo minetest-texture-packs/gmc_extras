594b4bb900c3b119d2f00d8548f96171eaf2d402
tacotexmex <josefsahlberg+minetest@gmail.com>
2017-05-02 20:18:49 +0200

    More correct sandstone block

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 --default overrides
 if minetest.get_modpath("default") ~= nil then
   minetest.override_item("default:sandstone_block", {
-  	tiles = {"default_sandstone_block.png",
+  	tiles = {"default_sandstone.png",
     "extras_default_sandstone_block_bottom.png",
     "extras_default_sandstone_block_side.png",
     "extras_default_sandstone_block_side.png",
