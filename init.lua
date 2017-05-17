2bcbb5b6ce0b841f331d08ed73ace7858ec6a015
tacotexmex <josefsahlberg+minetest@gmail.com>
2017-05-17 19:27:25 +0200

    Straw block

--- init.lua
+++ init.lua
@@ -1,14 +1,28 @@
+local sandstone_block = {
+  "default_sandstone.png",
+  "extras_default_sandstone_block_bottom.png",
+  "extras_default_sandstone_block_side.png",
+  "extras_default_sandstone_block_side.png",
+  "extras_default_sandstone_block_side.png",
+  "extras_default_sandstone_block_side.png"
+}
+
+local farming_straw = {
+  "extras_farming_straw_top_bottom.png",
+  "extras_farming_straw_top_bottom.png",
+  "farming_straw.png",
+  "farming_straw.png",
+  "farming_straw.png",
+  "farming_straw.png"
+}
+
 --default overrides
 if minetest.get_modpath("default") ~= nil then
   minetest.override_item("default:sandstone_block", {
-  	tiles = {"default_sandstone.png",
-    "extras_default_sandstone_block_bottom.png",
-    "extras_default_sandstone_block_side.png",
-    "extras_default_sandstone_block_side.png",
-    "extras_default_sandstone_block_side.png",
-    "extras_default_sandstone_block_side.png"},
+  	tiles = sandstone_block
   })
 end
+
 --bed overrides 
 if minetest.get_modpath("beds") ~= nil then
   minetest.register_craft({
@@ -17,5 +31,28 @@ if minetest.get_modpath("beds") ~= nil then
   		{"wool:green", "wool:green", "wool:white"},
   		{"group:wood", "group:wood", "group:wood"}
   	},
+  })
+end
+
+--farming overrides
+if minetest.get_modpath("farming") ~= nil then
+  minetest.override_item("farming:straw", {
+  	tiles = farming_straw
+  })
+end
+
+--stairs overrides
+if minetest.get_modpath("stairs") ~= nil then
+  minetest.override_item("stairs:slab_sandstone_block", {
+  	tiles = sandstone_block
+  })
+  minetest.override_item("stairs:stair_sandstone_block", {
+  	tiles = sandstone_block
+  })
+  minetest.override_item("stairs:slab_straw", {
+  	tiles = farming_straw
+  })
+  minetest.override_item("stairs:stair_straw", {
+  	tiles = farming_straw
   })
 end
